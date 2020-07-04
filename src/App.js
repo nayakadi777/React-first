ef2173e0d6a23322106b5f1bf40800d51373e989
Adity G Nayak <nayakadi77@gmail.com>
2020-07-04 17:18:05 +0530

    Menu component added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import logo from './logo.svg';
 import './App.css';
+import Menu from './components/MenuComponent/MenuComponent';
 
 function App() {
   return (
-    <div className="App">
+    <div >
     <Navbar dark color="primary">
       <div className="container">
         <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
       </div>
     </Navbar>
+    <Menu />
   </div>
   );
 }
