2b9cfe6cb4ecff418017e469237bc98cebf596c1
Adity G Nayak <nayakadi77@gmail.com>
2020-07-04 19:55:51 +0530

    changes to card and added card selection

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React , {Component}from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 import Menu from './components/MenuComponent/MenuComponent';
+import { DISHES } from './Shared/dishes';
 
-function App() {
+class App extends Component {
+   constructor(props){
+    super(props);
+    this.state ={
+      dishes:DISHES
+    }
+   }
+   render (){
+
+
   return (
     <div >
     <Navbar dark color="primary">
@@ -11,9 +21,11 @@ function App() {
         <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
       </div>
     </Navbar>
-    <Menu />
+    <Menu  dishes={this.state.dishes}/>
   </div>
   );
+}
+
 }
 
 export default App;
