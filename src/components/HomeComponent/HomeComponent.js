9e4952cee363a45c3ad4d6718aff271bb822c7ec
Adity G Nayak <nayakadi77@gmail.com>
2020-08-12 14:41:25 +0530

    Fetch Handling Errors

--- src/components/HomeComponent/HomeComponent.js
+++ src/components/HomeComponent/HomeComponent.js
@@ -5,6 +5,7 @@ import { Loading } from '../LoadingComponent/LoadingComponent';
 import { baseUrl } from '../../Shared/baseUrl';
 
 function RenderCard({item , isLoading, errMess}) {
+    console.log({item , isLoading, errMess});
     if (isLoading) {
         return(
                 <Loading />
@@ -30,12 +31,11 @@ function RenderCard({item , isLoading, errMess}) {
 }
 
 function Home(props) {
-    console.log(props)
     return(
         <div className="container">
             <div className="row align-items-start">
                 <div className="col-12 col-md m-1">
-                    <RenderCard item={props.dish} isLoading={props.dishesLoading} errMess={props.dishesErrMess} />
+                    <RenderCard item={props.dish} isLoading={props.dishesLoading} errMess={props.dishErrMess} />
                 </div>
                 <div className="col-12 col-md m-1">
                 <RenderCard item={props.promotion} isLoading={props.promoLoading} errMess={props.promoErrMess} />
